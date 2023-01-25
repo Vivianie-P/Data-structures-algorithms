957b5adc861b22c273d8b102af1b735567ca7175
Vivianie-P <vivianieprice@gmail.com>
2023-01-24 20:10:18 -0800

    Completed new problem

---
+++ AlgoProblems/bubbleSort.py
@@ -0,0 +1,19 @@
+from typing import List
+
+def bubbleSort(array):
+    sorted = False
+
+    while not sorted:
+        sorted = True
+        for x in range(len(array) - 1):
+            if array[x] > array[x+1]:
+                array[x], array[x+1] = array[x+1], array[x]
+                sorted = False
+    return array
+
+
+array = [8, 5, 2, 9, 5, 6, 3]
+print(bubbleSort(array))
+
+# array = [-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8]
+# print(bubbleSort(array))
--- LeetCodeProblems/1twoSum.py
+++ LeetCodeProblems/1twoSum.py
@@ -3,9 +3,10 @@ from typing import List
 """
 - Given a list and a target value this function finds the two elements in the
 list that are equal to the target value. 
+- Create a dictionary to hold the list with their index being the key and the number (or num) being the value. 
 - Loop through entire list keeping track of index and value
-- Checked the dictionary for the different of the target and each element
-- If found, return list of the diff's index from hashmap and current index.
+- Check the dictionary for the difference of the target and each element
+- If the difference is found in the dictionary, return list of the diff's index from hashmap and current index.
 - If not found, store element in the hashmap
 - Complexity: 
     Time - O(n) where n is the length of the list 
@@ -15,6 +16,8 @@ list that are equal to the target value.
 def twoSum(nums: List[int], target: int) -> List[int]:
         prevMap = {}
         
+        # this is loop is looping through the entire list
+        # keeping track of the index and the value of each num
         for i, n in enumerate(nums):
             diff = target - n
             if diff in prevMap:
