0dd8310b3ec4ec5ce7bfee53f6ec79fcce277035
Vivianie-P <vivianieprice@gmail.com>
2023-01-24 20:36:45 -0800

    Completed 2 new problems

--- AlgoProblems/bubbleSort.py
+++ AlgoProblems/bubbleSort.py
@@ -15,5 +15,5 @@ def bubbleSort(array):
 array = [8, 5, 2, 9, 5, 6, 3]
 print(bubbleSort(array))
 
-# array = [-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8]
-# print(bubbleSort(array))
+array = [-4, 5, 10, 8, -10, -6, -4, -2, -5, 3, 5, -4, -5, -1, 1, 6, -7, -6, -7, 8]
+print (bubbleSort(array))
