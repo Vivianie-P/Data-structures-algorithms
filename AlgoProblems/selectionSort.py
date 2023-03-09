616f462ac3d1b633b0ae83cf99287260cefdb0a4
Vivianie-P <vivianieprice@gmail.com>
2023-03-09 11:15:31 -0800

    Learning more about sorting alogrithms

--- AlgoProblems/selectionSort.py
+++ AlgoProblems/selectionSort.py
@@ -2,19 +2,24 @@ from typing import List
 
 def selectionSort(array):
     currentIdx = 0
-
+# this while loop is bringing the currentIdx to the len of the array - 1 
+# because once our currentIdx gets to the last idx in the list there
+# would be no more swaps to make because that last number will be in it's
+# final position.
     while currentIdx < len(array) - 1:
         smallestIdx = currentIdx
         for i in range(currentIdx + 1, len(array)):
             if array[smallestIdx] > array[i]:
                 smallestIdx = i
-        swap(currentIdx, smallestIdx, array)
+        # swap(currentIdx, smallestIdx, array)
+        array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
         currentIdx += 1
     return array
 
 
 def swap(i, j, array):
     array[i], array[j] = array[j], array[i]
+    # array[currentIdx], array[smallestIdx] = array[smallestIdx], array[currentIdx]
 
 array = [8, 5, 2, 9, 5, 6, 3]
 print(selectionSort(array))
